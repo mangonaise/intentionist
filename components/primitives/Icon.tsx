75b36b466f541a661beec96ae3d77bc35ca875a5
= <liam2robertson@gmail.com>
2021-09-20 23:41:40 +0100

    📦 add Icon and IconButton components

---
+++ components/primitives/Icon.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled'
+import { HTMLAttributes } from 'react'
+import { space, SpaceProps, typography, TypographyProps } from 'styled-system'
+
+interface CustomProps extends HTMLAttributes<HTMLDivElement> {
+  icon: () => JSX.Element
+}
+type StyleProps = SpaceProps & TypographyProps
+type IconProps = CustomProps & StyleProps
+
+const IconWrapper = styled.div<StyleProps>({
+  display: 'inline-flex',
+  verticalAlign: 'bottom'
+}, space, typography)
+
+const Icon = ({ icon, ...props }: IconProps) => {
+  const IconComponent = icon
+  return (
+    <IconWrapper {...props}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em">
+        <IconComponent />
+      </svg>
+    </IconWrapper>
+  )
+}
+
+export default Icon
---
+++ components/primitives/IconButton.tsx
@@ -0,0 +1,28 @@
+import Button, { ButtonProps } from './Button'
+import CenteredFlex from './CenteredFlex'
+import Icon from './Icon'
+
+interface Props extends ButtonProps {
+  icon: () => JSX.Element
+  right?: boolean
+}
+
+const iconMargin = '0.8em'
+
+const IconButton = ({ icon, right, children, ...props }: Props) => {
+  return (
+    <Button {...props}>
+      <CenteredFlex flexDirection={right ? 'row-reverse' : 'row'}>
+        <Icon
+          icon={icon}
+          ml={right && children ? iconMargin : 0}
+          mr={!right && children ? iconMargin : 0}
+          style={{ transform: 'translateY(-0.04rem) scale(1.35)' }}
+        />
+        {children}
+      </CenteredFlex>
+    </Button>
+  )
+}
+
+export default IconButton
