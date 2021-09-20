94fef2b00f588978d68d80820a6f0ecb8f68633a
= <liam2robertson@gmail.com>
2021-09-20 21:48:20 +0100

    🔧 make Button component extendable

--- components/primitives/Button.tsx
+++ components/primitives/Button.tsx
@@ -1,24 +1,29 @@
 import styled from '@emotion/styled';
+import { HTMLAttributes } from 'react';
 import { border, BorderProps, color, ColorProps, flexbox, FlexboxProps, space, SpaceProps, typography, TypographyProps } from 'styled-system';
 import theme from 'styles/theme';
 
-interface ButtonProps { bg?: string }
-type StyleProps = ButtonProps & SpaceProps & FlexboxProps & TypographyProps & ColorProps & BorderProps
+interface CustomProps extends HTMLAttributes<HTMLButtonElement> { 
+  bg?: string 
+}
+type StyleProps = SpaceProps & FlexboxProps & TypographyProps & ColorProps & BorderProps
+export type ButtonProps = StyleProps & CustomProps
 
-const background = ({ bg }: ButtonProps) => ({
+const background = ({ bg }: CustomProps) => ({
   backgroundColor: bg || theme.colors.button,
   '&:hover': {
     backgroundColor: bg || theme.colors.buttonHighlight
   }
 })
 
-const Button = styled.button<StyleProps>({
+const Button = styled.button<ButtonProps>({
   cursor: 'pointer',
   padding: '0.7rem 1rem',
+  border: 'none',
+  color: 'inherit',
   fontSize: 'inherit',
   fontFamily: 'inherit',
-  color: 'inherit',
-  border: 'none',
+  fontWeight: 450,
   borderRadius: theme.radii.default,
   backgroundColor: theme.colors.button,
 }, background, space, flexbox, typography, color, border)
