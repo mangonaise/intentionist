b398038c999bd66e5150d551906d9726c0865be7
= <liam2robertson@gmail.com>
2021-09-22 21:47:17 +0100

    🎨 add page padding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,6 @@ import authHandler from '@/lib/auth'
 import FadeIn from './primitives/FadeIn'
 import BackIcon from './icons/BackIcon'
 import IntentionistIcon from './icons/IntentionistIcon'
-import Box from './primitives/Box'
 import Flex from './primitives/Flex'
 import Heading from './primitives/Heading'
 import Icon from './primitives/Icon'
@@ -13,18 +12,16 @@ import Text from './primitives/Text'
 
 const Navbar = () => {
   return (
-    <FadeIn>
-      <Box pt={4} px={4} maxWidth="max" margin="auto">
-        <Flex alignItems="center" mb={[4]} py={[0, 0, 4]}>
-          <Icon icon={IntentionistIcon} fontSize={['2rem', '3rem']} mr={[4, 5, 6]} />
-          <Heading as="p" fontSize={['1.5rem', '2rem']} fontWeight={650} letterSpacing="0.03ch">
-            intentionist
-          </Heading>
-          <Spacer mr="auto" />
-          <Text mr={3} opacity={0.5}>{profileHandler().profileInfo?.displayName}</Text>
-          <IconButton onClick={authHandler.handleSignOut} icon={BackIcon} />
-        </Flex>
-      </Box>
+    <FadeIn maxWidth="max" margin="auto">
+      <Flex alignItems="center" mb={[4]} py={[0, 0, 4]}>
+        <Icon icon={IntentionistIcon} fontSize={['2rem', '3rem']} mr={[4, 5, 6]} />
+        <Heading as="p" fontSize={['1.5rem', '2rem']} fontWeight={650} letterSpacing="0.03ch">
+          intentionist
+        </Heading>
+        <Spacer mr="auto" />
+        <Text mr={3} opacity={0.5}>{profileHandler().profileInfo?.displayName}</Text>
+        <IconButton onClick={authHandler.handleSignOut} icon={BackIcon} />
+      </Flex>
     </FadeIn>
   )
 }
