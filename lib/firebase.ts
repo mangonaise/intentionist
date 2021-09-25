b7e4b5360272f1ce3a7de6a52cf4ec354f0691cd
= <liam2robertson@gmail.com>
2021-09-26 00:04:18 +0100

    🤫 suppress auth emulator warning during testing

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,6 +2,8 @@ import { initializeApp } from 'firebase/app'
 import { connectAuthEmulator, getAuth } from 'firebase/auth'
 import { connectFirestoreEmulator, getFirestore } from '@firebase/firestore'
 
+const consoleInfo = console.info
+
 const firebaseConfig = {
   apiKey: 'AIzaSyCfjsR8D8fm-na_IVemInUmEiGerYcSblk',
   authDomain: 'intentionist.firebaseapp.com',
@@ -15,6 +17,12 @@ export const auth = getAuth(firebaseApp)
 export const db = getFirestore(firebaseApp)
 
 if (typeof window === 'undefined' || window.location.hostname.includes('localhost')) {
-  connectAuthEmulator(auth, 'http://localhost:9099')
   connectFirestoreEmulator(db, 'localhost', 8080)
+  setShowAuthEmulatorWarning(false)
+  connectAuthEmulator(auth, 'http://localhost:9099')
+  setShowAuthEmulatorWarning(true)
+}
+
+function setShowAuthEmulatorWarning(show: boolean) {
+  console.info = show ? consoleInfo : () => { }
 }
