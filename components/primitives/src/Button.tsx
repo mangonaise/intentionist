f821bd54635207cdd06bba66a5e63fec1a990b21
= <liam2robertson@gmail.com>
2021-09-30 15:29:06 +0100

    📦 simplify Button primitive

--- components/primitives/src/Button.tsx
+++ components/primitives/src/Button.tsx
@@ -1,28 +1,16 @@
 import styled from '@emotion/styled';
 import { HTMLAttributes } from 'react';
-import { border, BorderProps, color, ColorProps, flexbox, FlexboxProps, layout, LayoutProps, space, SpaceProps, typography, TypographyProps } from 'styled-system';
+import { color, ColorProps, flexbox, FlexboxProps, layout, LayoutProps, space, SpaceProps, typography, TypographyProps } from 'styled-system';
 import theme from 'styles/theme';
 
 interface CustomProps extends HTMLAttributes<HTMLButtonElement> { 
   bg?: string,
   reduceHoverOpacity?: boolean
 }
-type StyleProps = SpaceProps & FlexboxProps & LayoutProps & TypographyProps & ColorProps & BorderProps
+type StyleProps = SpaceProps & FlexboxProps & LayoutProps & TypographyProps & ColorProps
 export type ButtonProps = StyleProps & CustomProps
 
-const custom = ({ bg, reduceHoverOpacity }: CustomProps) => ({
-  backgroundColor: bg || theme.colors.button,
-  '&:hover': {
-    backgroundColor: bg || theme.colors.buttonHighlight,
-    opacity: reduceHoverOpacity ? 0.9 : 1
-  },
-  '&:disabled': {
-    opacity: 0.3,
-    cursor: 'default'
-  }
-})
-
-const Button = styled.button<ButtonProps>({
+const Button = styled.button<ButtonProps>(({ bg, reduceHoverOpacity }: ButtonProps) => ({
   cursor: 'pointer',
   userSelect: 'none',
   padding: '0.7rem 1rem',
@@ -32,7 +20,15 @@ const Button = styled.button<ButtonProps>({
   fontFamily: 'inherit',
   fontWeight: 450,
   borderRadius: theme.radii.default,
-  backgroundColor: theme.colors.button,
-}, custom, space, layout, flexbox, typography, color, border)
+  backgroundColor: bg || theme.colors.button,
+  '&:hover': {
+    backgroundColor: bg || theme.colors.buttonHighlight,
+    opacity: reduceHoverOpacity ? 0.9 : 1
+  },
+  '&:disabled': {
+    opacity: 0.3,
+    cursor: 'default'
+  }
+}), space, layout, flexbox, typography, color)
 
 export default Button
