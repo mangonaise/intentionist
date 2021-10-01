3290cb1587b0781d0f00a0f298c20743988d3f80
= <liam2robertson@gmail.com>
2021-10-01 11:51:56 +0100

    🌐 update html page titles

--- pages/habits.tsx
+++ pages/habits.tsx
@@ -4,12 +4,14 @@ import { withApp } from '@/components/app'
 import { FadeIn } from '@/components/primitives'
 import { FilteredHabitsList, HabitsPageNavSection } from '@/components/page/habits'
 import HabitFilterHandler from '@/lib/logic/app/HabitFilterHandler'
+import Head from 'next/head'
 
 export const HabitFilterContext = createContext<HabitFilterHandler>(null!)
 
 const HabitsPage = () => {
   return (
     <HabitFilterContext.Provider value={container.resolve(HabitFilterHandler)}>
+      <Head><title>Habits</title></Head>
       <FadeIn maxWidth="habits" margin="auto">
         <HabitsPageNavSection />
         <FilteredHabitsList />
--- pages/habits/[id].tsx
+++ pages/habits/[id].tsx
@@ -4,6 +4,7 @@ import { withApp } from '@/components/app'
 import { FadeIn, Flex, Spacer } from '@/components/primitives'
 import { HabitEditorNavSection, HabitIconPicker, HabitNameInput, HabitStatusPicker } from '@/components/page/habit-editor'
 import HabitEditor from '@/lib/logic/app/HabitEditor'
+import Head from 'next/head'
 
 export const HabitEditorContext = createContext<HabitEditor>(null!)
 
@@ -12,6 +13,7 @@ const HabitEditorPage = () => {
   if (!editor.habit) return <></>
   return (
     <HabitEditorContext.Provider value={editor}>
+      <Head><title>{editor.isNewHabit ? 'Add habit' : 'Edit habit'}</title></Head>
       <FadeIn maxWidth="habits" margin="auto">
         <HabitEditorNavSection />
         <Spacer mb={[3, 4]} />
