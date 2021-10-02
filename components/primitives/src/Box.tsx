7b85a4fbc58f7854f31d33e39b7d975167f8723a
= <liam2robertson@gmail.com>
2021-10-02 22:15:49 +0100

    🔤 add typography props to Box

--- components/primitives/src/Box.tsx
+++ components/primitives/src/Box.tsx
@@ -1,10 +1,10 @@
 import styled from '@emotion/styled'
 import { HTMLAttributes } from 'react'
-import { background, BackgroundProps, border, BorderProps, color, ColorProps, flexbox, FlexboxProps, layout, LayoutProps, position, PositionProps, space, SpaceProps } from 'styled-system'
+import { background, BackgroundProps, border, BorderProps, color, ColorProps, flexbox, FlexboxProps, layout, LayoutProps, position, PositionProps, space, SpaceProps, typography, TypographyProps } from 'styled-system'
 
 type DivProps = HTMLAttributes<HTMLDivElement>
-type StyleProps = PositionProps  & SpaceProps & LayoutProps & FlexboxProps & ColorProps & BorderProps & BackgroundProps
+type StyleProps = PositionProps  & SpaceProps & LayoutProps & FlexboxProps & ColorProps & BorderProps & BackgroundProps & TypographyProps
 export type BoxProps = DivProps & StyleProps
-const Box = styled.div<StyleProps>(position, space, layout, flexbox, color, border, background)
+const Box = styled.div<StyleProps>(position, space, layout, flexbox, color, border, background, typography)
 
 export default Box
