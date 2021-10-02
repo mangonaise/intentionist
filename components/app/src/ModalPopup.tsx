0aadba804b977427ef60e558fb9a1b5e5cb9050e
= <liam2robertson@gmail.com>
2021-10-02 23:34:52 +0100

    📦 adjust modal popup centering

--- components/app/src/ModalPopup.tsx
+++ components/app/src/ModalPopup.tsx
@@ -5,6 +5,7 @@ import { CloseIcon } from '@/components/icons'
 import Modal from 'react-responsive-modal'
 import theme from 'styles/theme'
 import 'react-responsive-modal/styles.css'
+import css from '@styled-system/css'
 
 interface Props {
   isOpen: boolean,
@@ -15,21 +16,23 @@ interface Props {
 const ModalPopup = ({ isOpen, closeModal, children }: Props) => {
   return (
     <>
-      <Global styles={{
+      <Global styles={css({
         '.react-responsive-modal': {
           '&-modal': {
             backgroundColor: '#292929',
             borderRadius: theme.radii.default,
             boxShadow: 'none',
-            padding: 0
+            padding: 0,
+            marginBottom: ['15vh', '50vh']
           }
         }
-      }}
+      })}
       />
       <Modal
         open={isOpen}
         onClose={closeModal}
         closeIcon={<Icon icon={CloseIcon} color="whiteAlpha.40" fontSize="1.3rem" />}
+        center
         classNames={{
           overlay: 'modal-overlay'
         }}>
