092f02a10695a3f1ebc39405b3aa3ef0288f29e1
= <liam2robertson@gmail.com>
2021-10-04 20:40:11 +0100

    ✅ add check mark icon

---
+++ components/icons/src/CheckIcon.tsx
@@ -0,0 +1,12 @@
+const CheckIcon = () => {
+  return (
+    <path
+      d="M5 14l3.23 2.42a1 1 0 001.38-.16L18 6"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+    />
+  )
+}
+
+export default CheckIcon
