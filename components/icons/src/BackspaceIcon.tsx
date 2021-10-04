c7ca0fcff4ce013880e98842ac2788b3e07d7407
= <liam2robertson@gmail.com>
2021-10-04 01:01:31 +0100

    ⭐ add search, backspace icons

---
+++ components/icons/src/BackspaceIcon.tsx
@@ -0,0 +1,20 @@
+const BackspaceIcon = () => {
+  return (
+    <>
+      <path
+        d="M21 16c0 .94 0 1.41-.3 1.7-.3.3-.77.3-1.72.3H7.92c-.53 0-.8 0-1.01-.12-.23-.12-.38-.33-.67-.77l-2.7-4C3.18 12.57 3 12.3 3 12c0-.3.18-.57.54-1.1l2.7-4c.3-.45.44-.66.67-.78C7.13 6 7.39 6 7.92 6h11.06c.95 0 1.43 0 1.72.3.3.29.3.76.3 1.7v8z"
+        stroke="currentColor"
+        strokeWidth={2}
+      />
+      <path
+        d="M15 10l-4 4m0-4l4 4"
+        stroke="currentColor"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </>
+  )
+}
+
+export default BackspaceIcon
---
+++ components/icons/src/SearchIcon.tsx
@@ -0,0 +1,15 @@
+const SearchIcon = () => {
+  return (
+    <>
+      <circle cx={11} cy={11} r={7} stroke="currentColor" strokeWidth={2} />
+      <path
+        d="M11 8a3 3 0 00-3 3m12 9l-3-3"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+      />
+    </>
+  )
+}
+
+export default SearchIcon
