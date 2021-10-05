ea43b2a24684a6d09e803da373e32726bffe741a
= <liam2robertson@gmail.com>
2021-10-05 12:36:58 +0100

    📅 abbreviate table weekday names on small screens

--- components/page/home/src/WeekTable.tsx
+++ components/page/home/src/WeekTable.tsx
@@ -1,19 +1,13 @@
 import { CenteredFlex, Grid } from '@/components/primitives'
 import { Fragment } from 'react'
 import { ViewHabitsButton } from '..'
-import { HabitCell, StatusCell } from './table'
-
-const weekdayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+import { HabitCell, StatusCell, WeekdayCells } from './table'
 
 const WeekTable = () => {
   return (
     <Grid gridTemplateColumns="repeat(8, 1fr)" mx={['-1rem', 0]}>
       <CenteredFlex height="row" borderBottom="solid 1px" borderColor="grid"></CenteredFlex>
-      {weekdayNames.map((day) => (
-        <CenteredFlex height="row" borderBottom="solid 1px" borderColor="grid" key={day}>
-          {day}
-        </CenteredFlex>
-      ))}
+      <WeekdayCells />
       {Array.from({ length: 5 }).map((_, index) => (
         <Fragment key={index}>
           <HabitCell />
---
+++ components/page/home/src/table/src/WeekdayCells.tsx
@@ -0,0 +1,26 @@
+import { CenteredFlex } from '@/components/primitives'
+import { useMediaQuery } from '@/lib/hooks'
+
+const weekdaysLong = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+const weekdaysShort = ['M', 'T', 'W', 'T', 'F', 'S', 'S']
+
+const WeekdayCells = () => {
+  const weekdayNames = useMediaQuery<string[]>('(max-width: 600px)', weekdaysShort, weekdaysLong)
+
+  return (
+    <>
+      {weekdayNames.map((day, index) => (
+        <CenteredFlex
+          height="row"
+          borderBottom="solid 1px"
+          borderColor="grid"
+          key={index}
+        >
+          {day}
+        </CenteredFlex>
+      ))}
+    </>
+  )
+}
+
+export default WeekdayCells
