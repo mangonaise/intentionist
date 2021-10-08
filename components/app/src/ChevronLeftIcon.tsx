9831977561d658d4201b1c1aa14c50d79b36a2af
= <liam2robertson@gmail.com>
2021-10-08 15:40:58 +0100

    ⭐ add left/right chevron icons

---
+++ components/app/src/ChevronLeftIcon.tsx
@@ -0,0 +1,13 @@
+const ChevronLeftIcon = () => {
+  return (
+    <path
+      d="M15 6l-6 6 6 6"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  )
+}
+
+export default ChevronLeftIcon
---
+++ components/app/src/ChevronRightIcon.tsx
@@ -0,0 +1,13 @@
+const ChevronRightIcon = () => {
+  return (
+    <path
+      d="M9 6l6 6-6 6"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  )
+}
+
+export default ChevronRightIcon
