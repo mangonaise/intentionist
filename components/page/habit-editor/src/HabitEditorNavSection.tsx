73ea17a8ba97f1a483c18884efd6e899c670d6b7
= <liam2robertson@gmail.com>
2021-10-18 11:58:56 +0100

    🗑️ remove discard changes button on habit editor

--- components/page/habit-editor/src/HabitEditorNavSection.tsx
+++ components/page/habit-editor/src/HabitEditorNavSection.tsx
@@ -1,7 +1,7 @@
 import { observer } from 'mobx-react-lite'
 import { useContext } from 'react'
 import { HabitEditorContext } from 'pages/habits/[id]'
-import { Box, Button, Flex, IconButton, Text } from '@/components/primitives'
+import { Button, Flex, IconButton, Text } from '@/components/primitives'
 import { CloseIcon } from '@/components/icons'
 import { DeleteHabitDropdown } from '..'
 
@@ -10,12 +10,17 @@ const HabitEditorNavSection = () => {
 
   return (
     <Flex sx={{ pb: [3, 4], borderBottom: 'solid 1.5px', borderColor: 'divider' }}>
-      <IconButton icon={CloseIcon} onClick={editor.exit} />
-      <Box sx={{ alignSelf: 'center', ml: 4 }}>
-        <Text type="span" sx={{ color: 'whiteAlpha.70' }}>
-          {editor.isNewHabit ? 'Adding new ' : 'Editing '}habit
-        </Text>
-      </Box>
+      {editor.isNewHabit && <IconButton icon={CloseIcon} onClick={editor.exit} sx={{ mr: [2, 3, 4] }} />}
+      <Text
+        type="span"
+        sx={{
+          color: 'whiteAlpha.70',
+          alignSelf: 'center',
+          transform: editor.isNewHabit ? null : ['translateY(0.25rem)', 'translateY(0.4rem)']
+        }}
+      >
+        {editor.isNewHabit ? 'Adding new ' : 'Editing '}habit
+      </Text>
       <Button
         onClick={editor.saveAndExit}
         disabled={!editor.habit?.name}
