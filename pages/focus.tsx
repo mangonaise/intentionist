f3965696a07f0c111445780f550cb3a123d3b48b
= <liam2robertson@gmail.com>
2021-10-18 22:21:03 +0100

    🌐 add html title to focus page

--- pages/focus.tsx
+++ pages/focus.tsx
@@ -3,10 +3,12 @@ import { Flex, Heading, IconButton } from '@/components/primitives'
 import { FocusTimer } from '@/components/page/focus'
 import { BackIcon } from '@/components/icons'
 import NextLink from 'next/link'
+import Head from 'next/head'
 
 const FocusPage = () => {
   return (
     <Flex column sx={{ maxWidth: '500px', margin: 'auto' }}>
+      <Head><title>Focus</title></Head>
       <Flex align="center" sx={{ mb: [2, 3] }}>
         <NextLink href="/home">
           <IconButton icon={BackIcon} sx={{ mr: 3 }} />
