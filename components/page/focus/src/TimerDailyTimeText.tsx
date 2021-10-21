cec0f510b56dc358bcbaf488135a9dd683b3ada5
= <liam2robertson@gmail.com>
2021-10-21 12:04:55 +0100

    ⏲ update daily time text while timer runs

--- components/page/focus/src/TimerDailyTimeText.tsx
+++ components/page/focus/src/TimerDailyTimeText.tsx
@@ -7,16 +7,16 @@ import getWeekdayName from '@/lib/logic/utils/getWeekdayName'
 import useCurrentDay from '@/lib/hooks/useCurrentDay'
 
 const TimerDailyTimeText = () => {
-  const { getTimeSpentThisWeek, status, weekdayId: timerWeekdayId } = useContext(FocusTimerContext)
+  const { getTimeSpentThisWeek, progress, status, weekdayId: timerWeekdayId } = useContext(FocusTimerContext)
   const { weekdayId: todayId } = useCurrentDay()
 
   const weekdayIdToDisplay = status === 'not started' ? todayId : timerWeekdayId
   const weekdayName = getWeekdayName(weekdayIdToDisplay)
-  const time = getTimeSpentThisWeek(weekdayIdToDisplay)
+  const time = getTimeSpentThisWeek(weekdayIdToDisplay) + progress
 
   return (
     <Text type="span" sx={{ fontWeight: 'light' }}>
-      <Text type="span" sx={{ fontWeight: 'medium' }}>
+      <Text type="span" sx={{ fontWeight: 'medium', fontVariantNumeric: 'tabular-nums' }}>
         {time ? formatSeconds(time, 'letters') : 'No focused time'}
       </Text>
       {` on ${weekdayName}`}
