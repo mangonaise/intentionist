fe991f581df17897a0990d1ca5093da0c7df9ca7
= <liam2robertson@gmail.com>
2021-10-23 01:57:18 +0100

    🛠️ make habit cell a button instead of link

--- components/page/home/table/HabitCell.tsx
+++ components/page/home/table/HabitCell.tsx
@@ -8,6 +8,8 @@ import Flex from '@/components/primitives/Flex'
 import Link from '@/components/primitives/Link'
 import Dropdown from '@/components/app/Dropdown'
 import NextLink from 'next/link'
+import Button from '@/components/primitives/Button'
+import Text from '@/components/primitives/Text'
 
 const HabitCell = observer(({ habit }: { habit: Habit }) => {
   const { showHabitNames } = useContext(ColumnsDisplayContext)
@@ -30,31 +32,40 @@ const HabitCell = observer(({ habit }: { habit: Habit }) => {
 
 const HabitCellWithName = ({ habit }: { habit: Habit }) => {
   return (
-    <Flex
-      align="center"
-      sx={{
-        size: '100%',
-        pl: 2,
-        opacity: 0,
-        animation: 'fade-in forwards 600ms',
-      }}
-    >
-      <HabitCellIcon habitIcon={habit.icon} />
-      <NextLink href={`/habits/${habit.id}?returnHome=true`}>
-        <Link
+    <NextLink href={`/habits/${habit.id}?returnHome=true`}>
+      <Button
+        sx={{
+          size: '100%',
+          padding: 0,
+          background: 'none',
+          borderRadius: 0
+        }}
+      >
+        <Flex
+          align="center"
           sx={{
-            width: '100%',
-            maxWidth: 'min(30vw, 50ch)',
-            marginLeft: 3,
-            paddingY: '2px',
-            paddingRight: ['0.6em', 3],
-            overflowWrap: 'break-word'
+            paddingLeft: 2,
+            opacity: 0,
+            animation: 'fade-in forwards 600ms',
           }}
         >
-          {habit.name}
-        </Link>
-      </NextLink>
-    </Flex>
+          <HabitCellIcon habitIcon={habit.icon} />
+          <Text
+            type="span"
+            sx={{
+              width: '100%',
+              maxWidth: 'min(30vw, 50ch)',
+              marginLeft: 3,
+              paddingY: '2px',
+              paddingRight: ['0.6em', 3],
+              overflowWrap: 'break-word'
+            }}
+          >
+            {habit.name}
+          </Text>
+        </Flex>
+      </Button>
+    </NextLink>
   )
 }
 
