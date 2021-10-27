a39932d40c82dc7ba129b79487f3a36fa2210c29
= <liam2robertson@gmail.com>
2021-10-27 20:09:17 +0100

    📦 add Checkbox primitive

---
+++ components/primitives/Checkbox.tsx
@@ -0,0 +1,59 @@
+import { StyledComponent } from '@/components/types/StyledComponent'
+import { ComponentPropsWithoutRef } from 'react'
+
+interface CheckboxProps extends ComponentPropsWithoutRef<'input'> {
+  label: string
+}
+
+const Checkbox: StyledComponent<CheckboxProps> = (props) => {
+  const { label } = props
+
+  return (
+    <label
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        color: 'whiteAlpha.80',
+        '&:hover input:not(:checked)': {
+          backgroundColor: 'whiteAlpha.30',
+        }
+      }}
+    >
+      <input
+        type="checkbox"
+        sx={{
+          position: 'relative',
+          margin: 0,
+          marginRight: 2,
+          height: '1.15em',
+          minWidth: '1.15em',
+          appearance: 'none',
+          border: 'solid 2px',
+          borderColor: 'text',
+          borderRadius: 'default',
+          transition: 'background-color 150ms',
+          '&::before': {
+            content: '""',
+            position: 'absolute',
+            inset: '15%',
+            size: '70%',
+            clipPath: 'polygon(14% 44%, 0 65%, 50% 100%, 100% 16%, 80% 0%, 43% 62%)',
+            backgroundColor: 'bg',
+            opacity: 0,
+            transition: 'opacity 150ms'
+          },
+          '&:checked': {
+            backgroundColor: 'text',
+            '&::before': {
+              opacity: 1
+            }
+          }
+        }}
+        {...props}
+      />
+      {label}
+    </label>
+  )
+}
+
+export default Checkbox
