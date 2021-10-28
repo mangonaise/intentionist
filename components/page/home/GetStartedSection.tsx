22aa9a88b3ba1e78bc90475ade60f66d5ddaf3f5
= <liam2robertson@gmail.com>
2021-10-28 17:27:49 +0100

    ✨ add "get started" screen for new users

---
+++ components/page/home/GetStartedSection.tsx
@@ -0,0 +1,169 @@
+import { observer } from 'mobx-react-lite'
+import { Fragment } from 'react'
+import accentColor from '@/lib/logic/utils/accentColor'
+import useMediaQuery from '@/lib/hooks/useMediaQuery'
+import SmartEmoji from '@/components/app/SmartEmoji'
+import Flex from '@/components/primitives/Flex'
+import Heading from '@/components/primitives/Heading'
+import IconButton from '@/components/primitives/IconButton'
+import Text from '@/components/primitives/Text'
+import Grid from '@/components/primitives/Grid'
+import Box from '@/components/primitives/Box'
+import ArrowRightIcon from '@/components/icons/ArrowRightIcon'
+import NextLink from 'next/link'
+
+const GetStartedSection = observer(() => {
+  return (
+    <Flex center sx={{ mt: ['12.5vh', '3vh'] }}>
+      <Table />
+      <Flex column align="center" sx={{ textAlign: 'center' }}>
+        <Heading level={2} sx={{ fontSize: ['1.75rem', '2.75rem'], fontWeight: 'bold' }}>
+          Let's get started
+        </Heading>
+        <Text sx={{ mt: 3, mb: [2, 4], fontWeight: 'light' }}>
+          To begin, simply add the habits you'd like to track.
+        </Text>
+        <NextLink href="/habits">
+          <IconButton
+            right icon={ArrowRightIcon}
+            sx={{
+              marginTop: [3, 6],
+              backgroundColor: 'text',
+              color: 'bg',
+              fontWeight: 'medium'
+            }}
+            hoverEffect="opacity"
+          >
+            Start
+          </IconButton>
+        </NextLink>
+      </Flex>
+    </Flex>
+  )
+})
+
+const fakeHabits = [
+  {
+    icon: '⏰',
+    name: 'Wake up early',
+    statuses: [['✅'], ['💤'], ['✅'], ['✅']],
+  },
+  {
+    icon: '📚',
+    name: 'Read',
+    statuses: [['👍'], [], ['📕'], ['🤏']]
+  },
+  {
+    icon: '✍️',
+    name: 'Study',
+    statuses: [['👍'], ['⭐'], ['🤏'], []]
+  },
+  {
+    icon: '💦',
+    name: 'Exercise',
+    statuses: [['🏃'], ['💪'], [], ['🚶‍♂️']]
+  },
+  {
+    icon: '🌼',
+    name: 'Relax',
+    statuses: [['⭐'], ['👎'], ['👍'], ['⭐']]
+  },
+  {
+    icon: '🍎',
+    name: 'Healthy eating',
+    statuses: [['👍'], ['🥗'], [], ['👍']]
+  },
+  {
+    icon: '🙂',
+    name: 'Mood',
+    statuses: [['😊'], ['😬'], ['🙂'], ['🙂']]
+  }
+]
+
+const Table = () => {
+  const display = useMediaQuery('(max-width: 1000px)', false, true)
+
+  if (!display) return null
+
+  return (
+    <Box
+      sx={{
+        position: 'relative',
+        marginRight: '7.5rem',
+        userSelect: 'none',
+        '&::after': {
+          position: 'absolute',
+          content: '""',
+          inset: 0,
+          left: '60%',
+          backgroundImage: 'linear-gradient(to right, transparent, var(--background-color))'
+        }
+      }}
+    >
+      <Grid
+        sx={{
+          gridTemplateColumns: 'auto repeat(4, 6rem)',
+          borderBottom: 'solid 1px',
+          borderColor: 'grid'
+        }}
+      >
+        <div />
+        {['Mon', 'Tue', 'Wed', 'Thu'].map((day) => (
+          <FakeWeekdayCell day={day} key={day} />
+        ))}
+        {fakeHabits.map((habit) => <Fragment key={habit.name}>
+          <FakeHabitCell icon={habit.icon} name={habit.name} />
+          {habit.statuses.map((statuses, index) => <FakeStatusCell statuses={statuses} key={index} />)}
+        </Fragment>)}
+      </Grid>
+    </Box>
+  )
+}
+
+const FakeWeekdayCell = ({ day }: { day: string }) => {
+  return (
+    <Flex center sx={{ minHeight: 'row' }}>
+      {day}
+    </Flex>
+  )
+}
+
+const FakeHabitCell = ({ icon, name }: { icon: string, name: string }) => {
+  return (
+    <Flex
+      align="center"
+      sx={{
+        minHeight: 'row',
+        paddingRight: 4,
+        borderTop: 'solid 1px',
+        borderRight: 'solid 1px',
+        borderColor: 'grid'
+      }}
+    >
+      <SmartEmoji nativeEmoji={icon} rem={1.2} />
+      <Text type="span" sx={{ ml: 2 }}>{name}</Text>
+    </Flex>
+  )
+}
+
+const FakeStatusCell = ({ statuses }: { statuses: string[] }) => {
+  return (
+    <Flex
+      center
+      sx={{
+        borderTop: 'solid 1px',
+        borderRight: 'solid 1px',
+        borderColor: 'grid',
+        backgroundColor: statuses.length ? 'whiteAlpha.3' : null
+      }}
+    >
+      {statuses.map((emoji) => (
+        <Flex center sx={{ px: 1 }} key={emoji}>
+          <SmartEmoji nativeEmoji={emoji} rem={1.2} />
+        </Flex>
+      ))}
+    </Flex>
+  )
+}
+
+export default GetStartedSection
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,3 +1,5 @@
+import { container } from 'tsyringe'
+import HabitsHandler from '@/lib/logic/app/HabitsHandler'
 import withApp from '@/components/app/withApp'
 import NewWeekPrompt from '@/components/page/home/NewWeekPrompt'
 import WeekDropdown from '@/components/page/home/WeekDropdown'
@@ -5,12 +7,16 @@ import WeekIconDropdown from '@/components/page/home/WeekIconDropdown'
 import WeekTable from '@/components/page/home/WeekTable'
 import WeekViewModePicker from '@/components/page/home/WeekViewModePicker'
 import OpenFocusButton from '@/components/page/home/OpenFocusButton'
+import GetStartedSection from '@/components/page/home/GetStartedSection'
 import Box from '@/components/primitives/Box'
 import Flex from '@/components/primitives/Flex'
 import Spacer from '@/components/primitives/Spacer'
 import Head from 'next/head'
 
 const Home = () => {
+  const { habits } = container.resolve(HabitsHandler)
+  const userHasHabits = !!habits.length
+
   return (
     <Box sx={{ maxWidth: 'max', margin: 'auto' }}>
       <Head><title>Home</title></Head>
@@ -20,13 +26,15 @@ const Home = () => {
       <Flex sx={{ flexWrap: ['wrap', 'nowrap'] }}>
         <Flex sx={{ width: ['100%', 'fit-content'] }}>
           <WeekDropdown />
-          <Spacer mr={2} />
-          <WeekIconDropdown />
+          {userHasHabits && <>
+            <Spacer mr={2} />
+            <WeekIconDropdown />
+          </>}
         </Flex>
-        <OpenFocusButton />
+      {userHasHabits && <OpenFocusButton />}
       </Flex>
       <Spacer mb={[4, 6]} />
-      <WeekTable />
+      {userHasHabits ? <WeekTable /> : <GetStartedSection />}
     </Box>
   )
 }
