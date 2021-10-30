3645cf3be1c826af35d327feff84bc8e4094a4ab
= <liam2robertson@gmail.com>
2021-10-31 00:43:36 +0100

    ☁️️ add cloud sync indicator in navbar

--- components/app/Navbar.tsx
+++ components/app/Navbar.tsx
@@ -2,13 +2,17 @@ import { observer } from 'mobx-react-lite'
 import { container } from 'tsyringe'
 import { handleSignOut } from '@/lib/logic/utils/authUtilities'
 import ProfileHandler from '@/lib/logic/app/ProfileHandler'
+import DbHandler from '@/lib/logic/app/DbHandler'
 import accentColor from '@/lib/logic/utils/accentColor'
 import Dropdown from '@/components/app/Dropdown'
 import Box from '@/components/primitives/Box'
 import Flex from '@/components/primitives/Flex'
 import Heading from '@/components/primitives/Heading'
 import Icon from '@/components/primitives/Icon'
+import Spacer from '@/components/primitives/Spacer'
 import IntentionistIcon from '@/components/icons/IntentionistIcon'
+import CloudUploadingIcon from '@/components/icons/CloudUploadingIcon'
+import CloudSyncedIcon from '@/components/icons/CloudSyncedIcon'
 import SmartEmoji from './SmartEmoji'
 import theme from 'styles/theme'
 
@@ -17,40 +21,59 @@ const Navbar = () => {
     <Box sx={{ position: 'absolute', top: 2, left: 2, right: 2 }}>
       <Flex
         align="center"
-        sx={{
-          height: theme.navbarHeights,
-          maxWidth: 'max',
-          margin: 'auto',
-          paddingBottom: '0.5rem'
-        }}
+        sx={{ height: theme.navbarHeights, maxWidth: 'max', m: 'auto', paddingBottom: 2 }}
       >
-        <Icon
-          icon={IntentionistIcon}
-          sx={{
-            fontSize: ['1.6rem', '1.8rem', '2.1rem'],
-            ml: 2,
-            mr: [4, 4, 6],
-            color: accentColor.current,
-            transition: 'color 400ms',
-            transform: null
-          }}
-        />
-        <Heading
-          level={1}
-          sx={{
-            fontSize: ['1.5rem', '1.75rem', '2rem'],
-            fontWeight: 650,
-            letterSpacing: '0.03ch',
-            mr: 'auto'
-          }}
-        >
-          intentionist
-        </Heading>
+        <MainHeading />
+        <CloudSyncIndicator />
+        <Spacer mr={3} />
         <UserActionsDropdown />
       </Flex>
     </Box>
   )
 }
+
+const MainHeading = observer(() => {
+  return (
+    <>
+      <Icon
+        icon={IntentionistIcon}
+        sx={{
+          fontSize: ['1.6rem', '1.8rem', '2.1rem'],
+          ml: 2,
+          mr: [4, 4, 6],
+          color: accentColor.current,
+          transition: 'color 400ms',
+          transform: null
+        }}
+      />
+      <Heading
+        level={1}
+        sx={{
+          fontSize: ['1.5rem', '1.75rem', '2rem'],
+          fontWeight: 650,
+          letterSpacing: '0.03ch',
+          mr: 'auto'
+        }}
+      >
+        intentionist
+      </Heading>
+    </>
+  )
+})
+
+const CloudSyncIndicator = observer(() => {
+  const { isWriteComplete } = container.resolve(DbHandler)
+  return (
+    <Icon
+      icon={isWriteComplete ? CloudSyncedIcon : CloudUploadingIcon}
+      sx={{
+        transform: 'scale(1.6)',
+        opacity: isWriteComplete ? 0.5 : 1,
+        transition: 'opacity 150ms'
+      }}
+    />
+  )
+})
 
 const UserActionsDropdown = observer(() => {
   const { profileInfo } = container.resolve(ProfileHandler)
@@ -93,4 +116,4 @@ const UserActionsDropdown = observer(() => {
   )
 })
 
-export default observer(Navbar)
+export default Navbar
