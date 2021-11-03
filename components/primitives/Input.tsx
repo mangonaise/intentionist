594f4f78850a81a06f371d487f54b4d053049d70
= <liam2robertson@gmail.com>
2021-11-03 21:50:19 +0000

    📦 make Input component forward ref

--- components/primitives/Input.tsx
+++ components/primitives/Input.tsx
@@ -1,28 +1,30 @@
-import { StyledComponent } from '@/components/types/StyledComponent'
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithRef, forwardRef } from 'react'
 
-const Input: StyledComponent<ComponentPropsWithoutRef<'input'>> = (props) => (
-  <input
-    type="text"
-    sx={{
-      width: '100%',
-      margin: 0,
-      padding: '0.6rem 0.8rem',
-      fontFamily: 'inherit',
-      fontSize: 'inherit',
-      color: 'text',
-      background: 'rgba(255, 255, 255, 0.03)',
-      border: 'none',
-      borderBottom: 'solid 2px',
-      borderColor: 'whiteAlpha.30',
-      transition: 'border-color 150ms',
-      '&:focus': {
-        boxShadow: 'none',
-        borderColor: 'whiteAlpha.80'
-      }
-    }}
-    {...props}
-  />
-)
+const Input = forwardRef<HTMLInputElement, ComponentPropsWithRef<'input'>>(function Input(props, ref) {
+  return (
+    <input
+      type="text"
+      ref={ref}
+      sx={{
+        width: '100%',
+        margin: 0,
+        padding: '0.6rem 0.8rem',
+        fontFamily: 'inherit',
+        fontSize: 'inherit',
+        color: 'text',
+        background: 'rgba(255, 255, 255, 0.03)',
+        border: 'none',
+        borderBottom: 'solid 2px',
+        borderColor: 'whiteAlpha.30',
+        transition: 'border-color 150ms',
+        '&:focus': {
+          boxShadow: 'none',
+          borderColor: 'whiteAlpha.80'
+        }
+      }}
+      {...props}
+    />
+  )
+})
 
 export default Input
