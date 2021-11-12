06cd23f1db9a1ccffdbcbb240d7d7e0f145573af
= <liam2robertson@gmail.com>
2021-11-12 00:01:54 +0000

    🗃️ update denormalized data on profile updates

--- firebase/functions/src/triggers/onUpdateUserDocument.ts
+++ firebase/functions/src/triggers/onUpdateUserDocument.ts
@@ -1,59 +1,110 @@
+import type { Transaction } from 'firebase-admin/firestore'
+import type { BasicUserData } from '../types'
+import { firestore } from 'firebase-admin'
 import * as functions from 'firebase-functions'
 import admin = require('firebase-admin')
 
-type UserDocumentData = {
-  username: string,
-  displayName: string,
-  avatar: string
-}
+const db = admin.firestore()
 
 exports.onUpdateUserDocument = functions.firestore
   .document('users/{uid}')
-  .onWrite((change) => {
+  .onWrite(async (change, context) => {
     const { before, after } = change
+    const { uid } = context.params
+
+    const isNewUser = !change.before.exists
 
     if (!change.after.exists) {
       // the account was deleted - not implemented
       return null
     }
 
-    const oldData = {
-      username: before.get('username'),
-      displayName: before.get('displayName'),
-      avatar: before.get('avatar')
-    }
-
+    const oldUsername = before.get('username')
     const newData = {
       username: after.get('username'),
       displayName: after.get('displayName'),
       avatar: after.get('avatar')
     }
 
-    if (hasUserDocumentDataChanged(oldData, newData)) {
-      const db = admin.firestore()
-      const batch = db.batch()
+    await db.runTransaction(async (transaction) => {
+      if (!isNewUser) {
+        await updateDenormalizedData(transaction, uid, oldUsername, newData)
+      }
 
-      if (oldData.username !== newData.username) {
+      if (oldUsername !== newData.username) {
         // delete the old username document from the usernames collection
-        if (oldData.username) {
-          batch.delete(db.collection('usernames').doc(oldData.username))
+        if (oldUsername) {
+          transaction.delete(db.collection('usernames').doc(oldUsername))
         }
       }
 
       // update the username document
-      batch.set(db.collection('usernames').doc(newData.username), {
+      transaction.set(db.collection('usernames').doc(newData.username), {
         displayName: newData.displayName,
         avatar: newData.avatar
       })
-
-      return batch.commit()
-    }
+    })
 
     return true
   })
 
-function hasUserDocumentDataChanged(oldData: UserDocumentData, newData: UserDocumentData) {
-  return oldData.username !== newData.username
-    || oldData.displayName !== newData.displayName
-    || oldData.avatar !== newData.avatar
+async function updateDenormalizedData(transaction: Transaction, uid: string, oldUsername: string, newData: Omit<BasicUserData, 'uid'>) {
+  const getFriendsDocsToUpdate = async () => {
+    const collectionGroup = db.collectionGroup('userData').where(`friends.${uid}`, '!=', false)
+    return await transaction.get(collectionGroup)
+  }
+
+  const getIncomingFriendRequestsDocsToUpdate = async () => {
+    const collectionGroup = db.collectionGroup('userData').where(`incoming.${oldUsername}`, '!=', false)
+    return await transaction.get(collectionGroup)
+  }
+
+  const getOutgoingFriendRequestsDocsToUpdate = async () => {
+    const collectionGroup = db.collectionGroup('userData').where(`outgoing.${oldUsername}`, '!=', false)
+    return await transaction.get(collectionGroup)
+  }
+
+  const [friendsDocsToUpdate, incomingFriendRequestsDocsToUpdate, outgoingFriendRequestsDocsToUpdate] = await Promise.all([
+    getFriendsDocsToUpdate(),
+    getIncomingFriendRequestsDocsToUpdate(),
+    getOutgoingFriendRequestsDocsToUpdate()
+  ])
+
+  // --- update denormalized data ---
+
+  friendsDocsToUpdate.forEach((doc) => {
+    transaction.set(doc.ref, {
+      friends: {
+        [uid]: { ...newData }
+      }
+    }, { merge: true })
+  })
+
+  incomingFriendRequestsDocsToUpdate.forEach((doc) => {
+    const time = doc.data().incoming[oldUsername].time ?? 0
+    transaction.set(doc.ref, {
+      incoming: {
+        [oldUsername]: firestore.FieldValue.delete(),
+        [newData.username]: {
+          time,
+          displayName: newData.displayName,
+          avatar: newData.avatar
+        }
+      }
+    }, { merge: true })
+  })
+
+  outgoingFriendRequestsDocsToUpdate.forEach((doc) => {
+    const time = doc.data().outgoing[oldUsername].time ?? 0
+    transaction.set(doc.ref, {
+      outgoing: {
+        [oldUsername]: firestore.FieldValue.delete(),
+        [newData.username]: {
+          time,
+          displayName: newData.displayName,
+          avatar: newData.avatar
+        }
+      }
+    }, { merge: true })
+  })
 }
--- lib/tests/cloud-functions/onUpdateUserDocument.test.ts
+++ lib/tests/cloud-functions/onUpdateUserDocument.test.ts
@@ -7,8 +7,18 @@ import getDbShortcuts from '@/test-setup/getDbShortcuts'
 const { app, db } = getFirebaseAdmin()
 const {
   userDoc,
-  usernameDoc
+  usernameDoc,
+  friendsDoc,
+  friendRequestsDoc
 } = getDbShortcuts(db)
+
+const now = Date.now()
+let testUser = {
+  uid: 'test-onUpdateUserDocument',
+  username: `test_username${now}`,
+  displayName: 'onUpdateUserDocument Test User',
+  avatar: '🧪'
+}
 
 afterAll(async () => {
   await app.delete()
@@ -16,36 +26,33 @@ afterAll(async () => {
 
 // 🧪
 
-describe('onUpdateUserDocument background trigger function', () => {
-  const now = Date.now()
-  const uid = 'test-onUpdateUserDocument'
-  const originalUsername = `test_username${now}`
-  const updatedUsername = `test_username_updated${now}`
-  const testAvatarAndDisplayName = {
-    avatar: '🧪',
-    displayName: 'Firebase Admin'
-  }
-
+describe('automatically creating and updating username documents', () => {
   afterAll(async () => {
-    await userDoc(uid).delete()
-    await usernameDoc(updatedUsername).delete()
+    await userDoc(testUser.uid).delete()
+    await usernameDoc(testUser.username).delete()
   })
 
   test(`1. creating a user document automatically creates a username document with the user's avatar and display name`, async () => {
-    await userDoc(uid).set({
-      username: originalUsername,
-      ...testAvatarAndDisplayName
+    await userDoc(testUser.uid).set({
+      username: testUser.username,
+      displayName: testUser.displayName,
+      avatar: testUser.avatar
     })
 
     await waitForCloudFunctionExecution()
 
-    const generatedUsernameDoc = await usernameDoc(originalUsername).get()
-    expect(generatedUsernameDoc.data()).toEqual(testAvatarAndDisplayName)
+    const generatedUsernameDoc = await usernameDoc(testUser.username).get()
+    expect(generatedUsernameDoc.data()).toEqual({
+      displayName: testUser.displayName,
+      avatar: testUser.avatar
+    })
   })
 
   test('2. when a user changes their username, the old username document is deleted and a new one is created', async () => {
-    await userDoc(uid).update({
-      username: updatedUsername
+    const originalUsername = testUser.username
+    testUser.username = `test_username_updated${now}`
+    await userDoc(testUser.uid).update({
+      username: testUser.username
     })
 
     await waitForCloudFunctionExecution()
@@ -53,21 +60,164 @@ describe('onUpdateUserDocument background trigger function', () => {
     const originalUsernameDoc = await usernameDoc(originalUsername).get()
     expect(originalUsernameDoc.exists).toEqual(false)
 
-    const updatedUsernameDoc = await usernameDoc(updatedUsername).get()
-    expect(updatedUsernameDoc.data()).toEqual(testAvatarAndDisplayName)
+    const updatedUsernameDoc = await usernameDoc(testUser.username).get()
+    expect(updatedUsernameDoc.data()).toEqual({
+      displayName: testUser.displayName,
+      avatar: testUser.avatar
+    })
   })
 
   test('3. when a user updates their avatar or display name, the username document is updated with the new data', async () => {
-    const newAvatarAndDisplayName = {
-      avatar: '😎',
-      displayName: 'Cool Firebase Admin'
-    }
+    testUser.avatar = '😎'
+    testUser.displayName = 'onUpdateUserDocument Cool User'
 
-    await userDoc(uid).update(newAvatarAndDisplayName)
-
+    await userDoc(testUser.uid).update({ avatar: testUser.avatar, displayName: testUser.displayName })
     await waitForCloudFunctionExecution()
 
-    const updatedUsernameDoc = await usernameDoc(updatedUsername).get()
-    expect(updatedUsernameDoc.data()).toEqual(newAvatarAndDisplayName)
+    const updatedUsernameDoc = await usernameDoc(testUser.username).get()
+    expect(updatedUsernameDoc.data()).toEqual({
+      displayName: testUser.displayName,
+      avatar: testUser.avatar
+    })
+  })
+})
+
+describe('automatically updating denormalized data when a user updates their profile', () => {
+  beforeAll(async () => {
+    await userDoc(testUser.uid).set({
+      username: testUser.username,
+      displayName: testUser.displayName,
+      avatar: testUser.avatar
+    })
+    await waitForCloudFunctionExecution()
+  })
+
+  afterAll(async () => {
+    await userDoc(testUser.uid).delete()
+    await usernameDoc(testUser.username).delete()
+  })
+
+  describe('updating denormalized data in friendRequests doc', () => {
+    const otherUsername = 'other_username' // for ensuring proper merge
+    const senderUid = `test-request-sender-uid${now}`
+    const recipientUid = `test-request-recipient-uid${now}`
+
+    beforeEach(async () => {
+      await friendRequestsDoc(senderUid).set({
+        outgoing: {
+          [testUser.username]: { time: 123, displayName: testUser.displayName, avatar: testUser.avatar },
+          [otherUsername]: { time: 123 }
+        }
+      })
+
+      await friendRequestsDoc(recipientUid).set({
+        incoming: {
+          [testUser.username]: { time: 456, displayName: testUser.displayName, avatar: testUser.avatar },
+          [otherUsername]: { time: 123 }
+        }
+      })
+    })
+
+    afterEach(async () => {
+      await db.recursiveDelete(userDoc(senderUid))
+      await db.recursiveDelete(userDoc(recipientUid))
+    })
+
+    test('when a user updates their profile info, the friendRequests docs of anyone who has a request to or from that user will be updated', async () => {
+      testUser.avatar = '🐸'
+      testUser.displayName = 'onUpdateUserDocument Test Frog'
+      await userDoc(testUser.uid).update({ avatar: testUser.avatar, displayName: testUser.displayName })
+      await waitForCloudFunctionExecution()
+
+      expect((await friendRequestsDoc(senderUid).get()).data()).toEqual({
+        outgoing: {
+          [testUser.username]: {
+            time: 123,
+            displayName: testUser.displayName,
+            avatar: testUser.avatar
+          },
+          [otherUsername]: {
+            time: 123
+          }
+        }
+      })
+
+      expect((await friendRequestsDoc(recipientUid).get()).data()).toEqual({
+        incoming: {
+          [testUser.username]: {
+            time: 456,
+            displayName: testUser.displayName,
+            avatar: testUser.avatar
+          },
+          [otherUsername]: {
+            time: 123
+          }
+        }
+      })
+    })
+
+    test('when a user updates their username, the friendRequests docs of anyone who has a request to or from that user will be updated', async () => {
+      testUser.username = `a_very_cool_username${now}`
+      await userDoc(testUser.uid).update({ username: testUser.username })
+      await waitForCloudFunctionExecution()
+
+      expect((await friendRequestsDoc(senderUid).get()).data()).toEqual({
+        outgoing: {
+          [testUser.username]: {
+            time: 123,
+            displayName: testUser.displayName,
+            avatar: testUser.avatar
+          },
+          [otherUsername]: {
+            time: 123
+          }
+        }
+      })
+
+      expect((await friendRequestsDoc(recipientUid).get()).data()).toEqual({
+        incoming: {
+          [testUser.username]: {
+            time: 456,
+            displayName: testUser.displayName,
+            avatar: testUser.avatar
+          },
+          [otherUsername]: {
+            time: 123
+          }
+        }
+      })
+    })
+  })
+
+  describe('automatically updating denormalized user data in friends doc', () => {
+    const friendUid = `test-friend-uid${now}`
+    const otherUid = `other-uid${now}` // for ensuring proper merge
+
+    beforeEach(async () => {
+      await friendsDoc(friendUid).set({
+        friends: {
+          [testUser.uid]: { time: 123, username: testUser.username, displayName: testUser.displayName, avatar: testUser.avatar },
+          [otherUid]: { time: 123 }
+        }
+      })
+    })
+
+    afterEach(async () => {
+      await db.recursiveDelete(userDoc(friendUid))
+    })
+
+    test('when a user updates their profile info, the friends doc of anyone who is friends with that user will be updated', async () => {
+      testUser.username = `my_brand_new_username${now}`
+      testUser.avatar = '✨'
+      await userDoc(testUser.uid).update({ username: testUser.username, avatar: testUser.avatar })
+      await waitForCloudFunctionExecution()
+
+      expect((await friendsDoc(friendUid).get()).data()).toEqual({
+        friends: {
+          [testUser.uid]: { time: 123, username: testUser.username, displayName: testUser.displayName, avatar: testUser.avatar },
+          [otherUid]: { time: 123 }
+        }
+      })
+    })
   })
 })
