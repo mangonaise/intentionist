2679e041841297c4a5c63b498a330025ff2f544a
= <liam2robertson@gmail.com>
2021-11-17 16:38:31 +0000

    🗨️ adapt text when viewing friend's week
    the CondensedViewAlert no longer suggests "unhide to make changes"

--- components/page/home/table/CondensedViewAlert.tsx
+++ components/page/home/table/CondensedViewAlert.tsx
@@ -8,7 +8,7 @@ import Text from '@/components/primitives/Text'
 import UpLeftArrowIcon from '@/components/icons/UpLeftArrowIcon'
 
 const CondensedViewAlert = () => {
-  const { habitsInView, condenseView } = container.resolve(WeekHandler).weekInView
+  const { habitsInView, condenseView, friendUid } = container.resolve(WeekHandler).weekInView
 
   if (!condenseView || habitsInView.length > 0) return null
 
@@ -26,7 +26,7 @@ const CondensedViewAlert = () => {
     >
       <Icon icon={UpLeftArrowIcon} sx={{ mx: 6, transform: 'scale(1.8)' }} />
       <Text type="p" sx={{ color: accentColor.current, fontWeight: 'semibold', mt: 5 }}>
-        Empty rows are currently hidden. Unhide them to make changes.
+        Empty rows are currently hidden.{friendUid ? '' : ' Unhide them to make changes.'}
       </Text>
     </Flex>
   )
