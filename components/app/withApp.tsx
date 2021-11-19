7a58c4d76572b7c9c5202d40945614783d8d5734
= <liam2robertson@gmail.com>
2021-11-19 17:12:42 +0000

    ⚠️ warn unsaved changes on app level

--- components/app/withApp.tsx
+++ components/app/withApp.tsx
@@ -1,10 +1,12 @@
 import { observer } from 'mobx-react-lite'
 import { container } from 'tsyringe'
 import { useRouter } from 'next/dist/client/router'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import InitialFetchHandler from '@/logic/app/InitialFetchHandler'
+import DbHandler from '@/logic/app/DbHandler'
 import ProfileHandler from '@/logic/app/ProfileHandler'
 import useAutorun from '@/hooks/useAutorun'
+import useWarnUnsavedChanges from '@/hooks/useWarnUnsavedChanges'
 import FadeIn from '@/components/primitives/FadeIn'
 import Spacer from '@/components/primitives/Spacer'
 import withAuthUser from './withAuthUser'
@@ -15,9 +17,15 @@ import theme from 'styles/theme'
 
 const withApp = (WrappedComponent: () => JSX.Element) => withAuthUser(observer(() => {
   const router = useRouter()
+  const { isWriteComplete } = container.resolve(DbHandler)
   const { hasCompletedInitialFetches } = container.resolve(InitialFetchHandler)
   const [profileExists, setProfileExists] = useState(hasCompletedInitialFetches && !!container.resolve(ProfileHandler).profileInfo)
   const [fade] = useState(!hasCompletedInitialFetches)
+
+  useWarnUnsavedChanges({
+    unload: !isWriteComplete,
+    routeChange: false
+  }, 'Changes you made have not been synced yet. Are you sure you want to leave?')
 
   useAutorun(() => {
     if (hasCompletedInitialFetches) {
