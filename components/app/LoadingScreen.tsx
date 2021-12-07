e08e0071c59cfa56c90c543dd37839f3a6371295
= <liam2robertson@gmail.com>
2021-12-07 21:29:52 +0000

    🎨 add text to loading screen

--- components/app/LoadingScreen.tsx
+++ components/app/LoadingScreen.tsx
@@ -1,11 +1,13 @@
 import Flex from '@/components/primitives/Flex'
 import Icon from '@/components/primitives/Icon'
+import Text from '@/components/primitives/Text'
 import IntentionistIcon from '@/components/icons/IntentionistIcon'
 
 const LoadingScreen = () => {
   return (
-    <Flex center sx={{ position: 'fixed', left: 0, right: 0, top: 0, height: '90vh', opacity: 0.5 }}>
-      <Icon icon={IntentionistIcon} sx={{ fontSize: '2.5rem' }} />
+    <Flex center column sx={{ position: 'fixed', left: 0, right: 0, top: 0, height: '90vh', opacity: 0.5 }}>
+      <Icon icon={IntentionistIcon} sx={{ fontSize: '2.5rem', mb: 6 }} />
+      <Text>Just a moment</Text>
     </Flex>
   )
 }
